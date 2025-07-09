731259cc0e21c540c640c3106054f0e6322fc432
Official-Kallol <official.kallol2024@gmail.com>
2025-07-09 19:39:39 +0530

    Required catch-all route files & Optional catch-all route files

---
+++ app/files/[...filePath]/page.js
@@ -0,0 +1,9 @@
+export default async function File({ params }) {
+  console.log(await params);
+  const { filePath } = await params;
+  return (
+    <>
+      <h1>Required catch all route File/{filePath.join("/")}</h1>
+    </>
+  );
+}
---
+++ app/files2/[[...filesPath]]/page.js
@@ -0,0 +1,12 @@
+export default async function File2({ params }) {
+  console.log(await params);
+  const { filesPath } = await params;
+  return (
+    <>
+      <h1>Optional catch all route File/{filesPath?.join("/")}</h1>
+    </>
+  );
+}
+
+// Required catch all route work in the root folder
+// Optional catch all route not work in the root folder
