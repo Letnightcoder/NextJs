ff9a61ee2eb945d3c6e5b4a58b4183c2827259ad
Official-Kallol <official.kallol2024@gmail.com>
2025-07-11 12:00:28 +0530

    Data Fetching in Client Components in Next.js

---
+++ app/posts/page.js
@@ -0,0 +1,31 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+
+const post = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function featchPosts() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+            const data = await response.json();
+            setPosts(data);
+        }
+        featchPosts();
+    }, []);
+    return (
+        <>
+            <h1>Posts</h1>
+            <div className='post-container'>
+                {posts.map(({id, title, body}) => (
+                    <div className='post-card' key={id}>
+                        <h2>{title}</h2>
+                        <p>{body}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default post
