7db9ebb3408f4ee9cd2b012ed767a6275b553ab4
Official-Kallol <official.kallol2024@gmail.com>
2025-07-11 13:39:17 +0530

    add loading

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,31 +1,35 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const post = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        async function featchPosts() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
-            const data = await response.json();
-            setPosts(data);
-        }
-        featchPosts();
-    }, []);
-    return (
-        <>
-            <h1>Posts</h1>
-            <div className='post-container'>
-                {posts.map(({id, title, body}) => (
-                    <div className='post-card' key={id}>
-                        <h2>{title}</h2>
-                        <p>{body}</p>
-                    </div>
-                ))}
-            </div>
-        </>
-    );
-}
+  useEffect(() => {
+    async function featchPosts() {
+      // Add 4 second delay
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=5"
+      );
+      const data = await response.json();
+      setPosts(data);
+    }
+    featchPosts();
+  }, []);
+  return (
+    <>
+      <h1>Posts</h1>
+      <div className="post-container">
+        {posts.map(({ id, title, body }) => (
+          <div className="post-card" key={id}>
+            <h2>{title}</h2>
+            <p>{body}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default post
+export default post;
